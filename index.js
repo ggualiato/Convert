9e28509418b50f37fbba130c8d7f96564e4a5acc
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2019-01-09 13:53:12 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron
+
+let mainWindow
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: { backgroundThrottling: false }
+    })
+
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+})
